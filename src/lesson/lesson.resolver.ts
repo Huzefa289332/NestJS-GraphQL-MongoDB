3e65dc8374957ece94a96b281f3df784ca716a59
Huzefa Battiwala <huzefa.battiwala@cubixlabs.com>
2021-06-10 20:26:26 +0500

    Init

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,50 @@
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
+import { StudentService } from 'src/student/student.service';
+import { AssignStudentsToLessonInput, CreateLessonInput } from './lesson.input';
+import { LessonService } from './lesson.service';
+import { LessonType } from './lesson.type';
+
+@Resolver((of) => LessonType)
+export class LessonResolver {
+  constructor(
+    private lessonService: LessonService,
+    private studentService: StudentService,
+  ) {}
+  @Query((returns) => LessonType)
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
+  }
+
+  @Query((returns) => [LessonType])
+  lessons() {
+    return this.lessonService.getLessons();
+  }
+
+  @Mutation((returns) => LessonType)
+  createLesson(
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
+  ) {
+    return this.lessonService.createLesson(createLessonInput);
+  }
+
+  @Mutation((returns) => LessonType)
+  assignStudentsToLesson(
+    @Args('assignStudentsToLessonInput')
+    assignStudentsToLessonInput: AssignStudentsToLessonInput,
+  ) {
+    const { lessonId, studentIds } = assignStudentsToLessonInput;
+    return this.lessonService.assignStudentsToLesson(lessonId, studentIds);
+  }
+
+  @ResolveField()
+  students(@Parent() lesson: LessonType) {
+    return this.studentService.getManyStudents(lesson.students);
+  }
+}
